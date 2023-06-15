5f0d07d425fb83823b31b925fe241413f6b82586
AfonsoVip <93103014+AfonsoVip@users.noreply.github.com>
2023-06-15 13:25:31 +0100

    Update session.py

--- session.py
+++ session.py
@@ -1,11 +1,35 @@
 import os
+import sqlite3
 import time
 import shutil
+import json
 
 class Session:
-    def __init__(self):
-        self.history = []
+    def __init__(self, db_path="results.db"):
+        self.conn = sqlite3.connect(db_path)
+        self._create_table()
 
-    def add_result(self, file_name, timestamp, result):
-        entry = {'file_name': file_name, 'timestamp': timestamp, 'result': result}
-        self.history.append(entry)
+    def _create_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS history (
+                id INTEGER PRIMARY KEY,
+                file_name TEXT,
+                timestamp TEXT,
+                threshold REAL,
+                result TEXT
+            )
+        """)
+        self.conn.commit()
+
+    def add_result(self, file_name, timestamp, threshold, result):
+        cursor = self.conn.cursor()
+        cursor.execute('''INSERT INTO history (file_name, timestamp, threshold, result) VALUES (?, ?, ?, ?)''', (file_name, timestamp, threshold, json.dumps(result)))
+        self.conn.commit()
+
+    def get_history(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM history")
+        records = cursor.fetchall()
+        history = [{'id': row[0], 'file_name': row[1], 'timestamp': row[2], 'threshold': row[3], 'result': json.loads(row[4])} for row in records]
+        return history
