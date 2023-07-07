8b1c83fa2856dc1518f6ec63f01bc5bceee90af7
AfonsoVip <93103014+AfonsoVip@users.noreply.github.com>
2023-07-07 23:40:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1523,7 +1523,6 @@ if selected_tab == "History":
 
             result_data = history_results[index]['result']
             selected_result = {
-                'df_thresholds': pd.read_json(result_data['df_thresholds'], orient='split'),
                 'first_strategy_df': pd.read_json(result_data['first_strategy_df'], orient='split'),
                 'second_strategy_df': pd.read_json(result_data['second_strategy_df'], orient='split'),
                 'third_strategy_df': pd.read_json(result_data['third_strategy_df'], orient='split'),
@@ -1533,14 +1532,18 @@ if selected_tab == "History":
                 'fig3': plotly.io.from_json(result_data['fig3']),
             }
             
+            if calculate_thresholds and 'df_thresholds' in result_data:
+                selected_result['df_thresholds'] = pd.read_json(result_data['df_thresholds'], orient='split')
+                
             st.markdown("<br>", unsafe_allow_html=True)
             st.markdown("<br>", unsafe_allow_html=True)
             st.markdown(f'<p style="color:#3dfd9f;font-size: 20px;">Results for file : {history_results[index]["file_name"]} at {history_results[index]["timestamp"]}.</p>', unsafe_allow_html=True)
 
-            st.markdown("<h3 style='color: #3dfd9f;font-size: 20px;'>Threshold Summary</h3>", unsafe_allow_html=True)
-            # Apply table_style to df_thresholds
-            df_thresholds_html = selected_result['df_thresholds'].to_html(classes="dataframe",index=False)
-            st.write(f'{table_style}{df_thresholds_html}', unsafe_allow_html=True)
+            if calculate_thresholds and 'df_thresholds' in selected_result:
+                    st.markdown("<h3 style='color: #3dfd9f;font-size: 20px;'>Threshold Summary</h3>", unsafe_allow_html=True
+                    # Apply table_style to df_thresholds
+                    df_thresholds_html = selected_result['df_thresholds'].to_html(classes="dataframe",index=False)
+                    st.write(f'{table_style}{df_thresholds_html}', unsafe_allow_html=True)
 
             st.plotly_chart(selected_result['fig1'])
             st.plotly_chart(selected_result['fig2'])
